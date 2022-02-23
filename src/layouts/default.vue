e9500d38206134386abceb1593a4b3b5fed5de66
Aaric <aaricchen@foxmail.com>
2022-02-23 13:26:57 +0800

    增加默认布局

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,8 +1,31 @@
-<!-- 默认页面布局 -->
 <template>
   <v-app>
-    <div class="pa-2 pa-md-4 flex-grow-1 align-center justify-center d-flex flex-column">
-      <nuxt />
-    </div>
+    <v-main>
+      <v-container class="fill-height">
+        <v-layout>
+          <nuxt />
+        </v-layout>
+      </v-container>
+    </v-main>
+    <v-bottom-navigation grow fixed>
+      <v-btn to="/">
+        <div class="mt-1">首页</div>
+        <icon small icon="home" />
+      </v-btn>
+    </v-bottom-navigation>
   </v-app>
 </template>
+
+<script>
+import Icon from "@/components/common/Icon";
+
+export default {
+  components: { Icon },
+};
+</script>
+
+<style scoped>
+main {
+  margin-bottom: 56px;
+}
+</style>
