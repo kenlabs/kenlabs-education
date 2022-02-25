21d2927e8c7d8fc3dbfcf99a41f71b547a129ce4
Aaric <aaricchen@foxmail.com>
2022-02-25 16:24:11 +0800

    增加图标文本组件

---
+++ src/components/common/IconText.vue
@@ -0,0 +1,28 @@
+<!-- 带图标的文本组件 -->
+<template>
+  <div class="d-inline-flex align-center">
+    <icon v-if="icon" full-width x-small :color="color" :icon="icon" />
+    <slot name="icon" />
+    <slot />
+  </div>
+</template>
+
+<script>
+import Icon from "~/components/common/Icon";
+
+export default {
+  name: "IconText",
+  components: { Icon },
+  props: {
+    icon: String,
+    color: String,
+    advanceIcon: Boolean,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+i.v-icon {
+  margin-right: 4px;
+}
+</style>
