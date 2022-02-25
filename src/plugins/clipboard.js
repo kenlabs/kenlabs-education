b114f1a17b4206049523aa0324d28f333597a3e3
Aaric <aaricchen@foxmail.com>
2022-02-25 18:29:47 +0800

    修改粘贴板插件问题

--- src/plugins/clipboard.js
+++ src/plugins/clipboard.js
@@ -1,16 +1,9 @@
-import Vue from 'vue'
+import Vue from "vue";
 
 /**
- * Copy to clipboard the text passed
- * @param {String} text string to copy
- * @param {String} toastText message to appear on the toast notification
+ * 复制粘贴工具
+ * @param text 要复制的文本
  */
 Vue.prototype.$clipboard = function (text) {
-  const el = document.createElement('textarea')
-
-  el.value = text
-  document.body.appendChild(el)
-  el.select()
-  document.execCommand('copy')
-  document.body.removeChild(el)
-}
+  return window.navigator.clipboard.writeText(text);
+};
